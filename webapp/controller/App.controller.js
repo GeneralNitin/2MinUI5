8db7241bc9c5fe8678b671e880af62add89d40c5
Nitin Kumar <nitin.kumar15@sap.com>
2022-02-27 15:43:09 +0530

    feat: JSON modle and Data Binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
     return Controller.extend("sap.ui.demo.controller.App", {
         incrementBy1: function() {
             let myTextElem = this.getView().byId("counter");
             let myNum = parseInt(myTextElem.getText());
             myNum += 1;
             myTextElem.setText(myNum);
+        },
+
+        /**
+         * @override
+         */
+        onInit: function() {
+            let oModel = new JSONModel(
+                sap.ui.require.toUrl("sap/ui/demo/Data.json")
+            );
+            this.getView().setModel(oModel);
         }
     })
 });
