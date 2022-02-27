ff446ef60029b47be1dc8cf55e0018d335614ce1
Nitin Kumar <nitin.kumar15@sap.com>
2022-02-27 15:29:30 +0530

    refactor: move App.view in view folder

--- webapp/index.js
+++ webapp/index.js
@@ -1,16 +1,11 @@
 sap.ui.define([
-    "sap/m/Button",
     "sap/ui/core/mvc/XMLView"
-], function(Button, XMLView) {
+], function(XMLView) {
     "use strict";
 
     XMLView.create({
-        viewName: "sap.ui.demo.App"
+        viewName: "sap.ui.demo.view.App"
     }).then(function (oView) {
         oView.placeAt("content");
     })
-
-    new Button({
-        text:"Hello World"
-    }).placeAt("content");
 })
